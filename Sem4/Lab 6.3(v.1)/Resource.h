fc6821183b4650da0a8d415f5f69760078276837
Mr-big-cake <63250784+Mr-big-cake@users.noreply.github.com>
2020-08-11 00:45:57 +0600

    Update Resource.h

--- Sem4/Lab 6.3(v.1)/Resource.h
+++ Sem4/Lab 6.3(v.1)/Resource.h
@@ -1,70 +1,36 @@
-﻿
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Lab 6.3(v.1).rc
+#define TIMER_ID 200
+#define IDM_APPLY_ON 201
+#define IDM_APPLY_EXIT 202
+#define IDM_STOP 203
 
-#define IDS_APP_TITLE                   111
 
-#define IDD_ABOUTBOX                    101
 
-#define IDI_LAB61V2                     103
-#define IDI_SMALL                       104
+#define IDS_APP_TITLE			103
 
-#define IDR_MAINFRAME                   105
-
-#define IDC_MYICON                      106
-#define IDC_LAB61V2                     107
-#define IDC_STATIC                      108
-
-#define IDM_SETDATETIME					201
-#define IDM_DEFAULT_TIMER				202
-#define IDM_ABOUT                       203
-#define IDM_EXIT                        204
-#define IDM_APPLY 205
-#define IDM_UPDATE_TIMER 206
-
-#define MAX_LOADSTRING  100
-
-#define WINDOW_X 500
-#define WINDOW_Y 225
-#define WINDOW_W 300
-#define WINDOW_H 260
-
-#define STATIC_DAT_X 15
-#define	STATIC_DAT_Y 10
-#define STATIC_DAT_W 250
-#define STATIC_DAT_H 25
-
-#define EDIT_DAT_X 15
-#define EDIT_DAT_Y 35
-#define EDIT_DAT_W 250
-#define EDIT_DAT_H 20
-
-#define STATIC_T_X 15
-#define STATIC_T_Y 75
-#define STATIC_T_W 250
-#define STATIC_T_H 20
-
-#define EDIT_T_X 15
-#define EDIT_T_Y 100
-#define EDIT_T_W 250
-#define EDIT_T_H 20
-
-#define BUTTON_X 100
-#define BUTTON_Y 135
-#define BUTTON_W 75
-#define BUTTON_H 30
-
-#define CLOCK_X 15
-#define CLOCK_Y 175
-#define CLOCK_W 250
-#define CLOCK_H 20
-
-// Next default values for new objects
-// 
+#define IDR_MAINFRAME			128
+#define IDD_LAB63V1_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_LAB63V1			107
+#define IDI_SMALL				108
+#define IDC_LAB63V1			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Следующие стандартные значения для новых объектов
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           110
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
 #endif
 #endif
